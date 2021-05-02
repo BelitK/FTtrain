f7a4d3d42d20e16538ea26d78be714c5500b1dbe
Belit <belitberdelk@gmail.com>
2021-05-02 23:45:05 +0300

    changed info.dat file

--- addYolShort.py
+++ addYolShort.py
@@ -2,7 +2,7 @@ import os
 import time
  
 file_text = open("short.dat", "r")
-i=0
+i=798
 for file in file_text:
     tt_S =""
     yolo = open(file[:-1],"r")
@@ -10,7 +10,7 @@ for file in file_text:
     for info in data:
         tt_S += info[1:]
         a = tt_S.replace("\n","")
-    line = f"{file[:-3]}.jpg {len(data)}{a}\n"
+    line = f"{file[6:-3]}.jpg {len(data)}{a}\n"
     with open('info.dat', 'a') as f:
              f.write(f"{line}")
     i+=1
